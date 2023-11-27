2326a0c62604051f81428b2367de0e4eb944c7d9
hawalinajwan <muhammadhawalinajwan@gmail.com>
2023-11-27 20:47:30 +0700

    ee

--- theme.config.js
+++ theme.config.js
@@ -2,9 +2,18 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
+    <small className="fabrand" style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © hawalinajwan.
-      <a href="/feed.xml">RSS</a>
+      <a href="/feed.xml">feed</a>
+      <ul className="about">
+        <li className="fabrands"><a href="https://instagr.am/hawalinajwan_" target="_blank" className='instagram'></a>
+        </li>
+        <li><a href="https://open.spotify.com/user/fnskc5zkcs6mtf4s2wxw0a88o?si=0a00086fafbd4a23"
+          target="_blank" className='fabrands spotify'></a></li>
+        <li><a href="https://discord.com/users/644892716953632768" target="_blank"
+          className="fabrands discord"></a></li>
+        <li><a href="https://github.com/hawalinajwan" target="_blank" className='fabrands github'></a></li>
+      </ul>
       <style jsx>{`
         a {
           float: right;
